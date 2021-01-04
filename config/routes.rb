c1c8a7a81d7616cbc3e47910ae9e6c510c4c5088
kobaltz <dave@k-innovations.net>
2021-01-03 23:38:11 -0500

    277

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'welcome#index'
-  get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products
+  root to: 'products#index'
 end
---
+++ db/migrate/20210103025149_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
